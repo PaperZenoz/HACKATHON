0d2a291a7c758bb30fbcef0ee48c9429060807ee
vladimir.petrulevich <etu21b@yandex.ru>
2023-11-18 15:44:13 +0700

    Правки вёрстки

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,18 +1,19 @@
 $(window).on("load", function () {
     $('#preloader').fadeOut(500)
     $('body').css("opacity", 1)
- 
- });
- 
- $(document).ready(function () {
+
+});
+
+$(document).ready(function () {
     function faq() {
-       const $head = $('.faq__head')
- 
-       $head.on('click', function () {
-          $(this).siblings('.faq__body').fadeToggle()
-       })
+        const $head = $('.faq__head')
+
+        $head.on('click', function () {
+            $(this).toggleClass('open')
+            $(this).siblings('.faq__body').fadeToggle()
+        })
     }
- 
+
     faq()
- 
- })
+
+})
